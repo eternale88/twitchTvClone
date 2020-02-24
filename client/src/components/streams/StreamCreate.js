223327b8d799f4d504113320eace95e98db5cf31
eternale88 <ethanrobbins@ymail.com>
2020-02-23 19:51:18 -0500

    start create stream endpoint

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Field, reduxForm } from 'redux-form'
+import { connect } from 'react-redux'
+import { createStream } from '../../actions'
 
 class StreamCreate extends React.Component {
   renderError(error, touched) {
@@ -28,9 +30,9 @@ class StreamCreate extends React.Component {
     )
   }
 
-  onSubmit(formValues) {
+  onSubmit = (formValues) => {
     // redux form automatically calls prevent default
-    console.log(formValues)
+    this.props.createStream(formValues)
   }
   render() {
     return (
@@ -63,7 +65,9 @@ const validate = (formValues) => {
   return errors
 }
 
-export default reduxForm({
+const formWrapped = reduxForm({
   form: 'streamCreate',
   validate: validate
 })(StreamCreate)
+
+export default connect(null, { createStream })(formWrapped)
